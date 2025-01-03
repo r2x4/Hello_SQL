0884c7caff4d4f61144cf47e40b59a60c3a9d529
Rodrigo <rrs230780@gmail.com>
2025-01-02 20:52:27 -0500

    noveno

---
+++ Join/01_inner_join.sql
@@ -0,0 +1,44 @@
+-- datos comunes de dos tablas
+-- esto es para 1:1
+
+SELECT * FROM users
+INNER JOIN  dni
+ON users.user_id = dni.user_id;
+
+SELECT * FROM users
+JOIN  dni
+ON users.user_id = dni.user_id;-- es igual Inner Join al Join llaman las misma informacion
+
+-- se puede hacer tablas ejemplo
+
+SELECT name, last_name, age, email, dni_number FROM users
+JOIN  dni
+ON users.user_id = dni.user_id
+ORDER BY age ASC;-- se pueden hacer consultas internas aprende cabezon
+
+-- esto N:N 
+
+SELECT * FROM users
+JOIN Companies
+ON users.company_id = Companies.company_id;
+
+SELECT * FROM Companies
+JOIN users
+ON users.company_id = Companies.company_id;
+
+-- se hace tabla con datos especiales
+SELECT Companies.name, users.name, last_name FROM Companies
+JOIN users
+ON users.company_id = Companies.company_id;
+
+-- varias tablas 
+
+SELECT users_languages
+JOIN users ON users_languages.user_id = users.user_id
+JOIN languages ON users_languages.language_id = languages.language_id;
+
+-- aqui es el mismo anterior pero llamo columnas y les cambio el nombre
+
+SELECT users.name AS nombre, last_name AS apellidos, languages.name AS lenguaje_de_programacion FROM users_languages
+JOIN users ON users_languages.user_id = users.user_id
+JOIN languages ON users_languages.language_id = languages.language_id;
