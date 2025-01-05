5953c905162fe8d083a2810defc03a5afb1e0bec
Rodrigo <rrs230780@gmail.com>
2025-01-04 21:04:09 -0500

    duodecimo

--- Advance/01_index.sql
+++ Advance/01_index.sql
@@ -1,2 +1,15 @@
 -- es un indice o simil indice en un libro
+CREATE INDEX idx_name ON users(name);
 
+CREATE UNIQUE INDEX idx_name ON users(name);
+
+CREATE UNIQUE INDEX idx_name ON users(name, last_name);
+
+-- borrar el index
+
+DROP INDEX idx_name ON users;
+
+
+-- el llamadado despues de index
+SELECT * FROM users WHERE name = "Rodrigo";
+-- el hace referencia para una columna o varias es como para sacer un  dato concreto
---
+++ Advance/02_trigger.sql
@@ -0,0 +1,4 @@
+-- es un disparador 
+-- sirve ejemplo que cuado se actualice un mail lo guardara en otra base de datos y deja el actual
+
+SELECT * FROM users;
