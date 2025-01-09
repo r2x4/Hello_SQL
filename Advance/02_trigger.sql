9efc9ee920abd5ab13f356f83cc4b8a526eec638
Rodrigo <rrs230780@gmail.com>
2025-01-09 12:21:06 -0500

    trece

--- Advance/02_trigger.sql
+++ Advance/02_trigger.sql
@@ -1,4 +1,18 @@
 -- es un disparador 
 -- sirve ejemplo que cuado se actualice un mail lo guardara en otra base de datos y deja el actual
 
-SELECT * FROM users;
+CREATE TABLE `email_history` (
+  `id_email_history` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_email_history`)
+  UNIQUE INDEX 'email_history_id_UNIQUE'('email_history_id' ASC) VISIBLE);
+
+
+  CREATE TRIGGER tg_email
+  BEFORE/AFTER INSERT/UPDATE/DELETE-- son las opciones antes despues ect
+  ON users
+
+  CREATE TRIGGER tg_email
+  AFTER UPDATE -- antes de carga
+  ON users
