1bf3069fa5e29820f188bd36eb93b2a6bfb8561f
Rodrigo Rocha Sierra <rrs230780@gmail.com>
2025-04-30 13:06:13 -0500

    guradar

--- postgres/create_table.sql
+++ postgres/create_table.sql
@@ -1,9 +1,13 @@
 CREATE SCHEMA IF NOT EXISTS prueba;
 
-CREATE TABLE prueba.clientes (
-    id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100),
-    correo VARCHAR(150),
-    telefono VARCHAR(20),
-    fecha_registro DATE DEFAULT CURRENT_DATE
-);
+CREATE TABLE
+    prueba.clientes (
+        id SERIAL PRIMARY KEY,
+        nombre VARCHAR(100),
+        correo VARCHAR(150),
+        telefono VARCHAR(20),
+        fecha_registro DATE DEFAULT CURRENT_DATE
+    );
+
+-- crear base de datos ojo se hace en donde dice bases de datos
+CREATE DATABASE nombre_rodrigo;
